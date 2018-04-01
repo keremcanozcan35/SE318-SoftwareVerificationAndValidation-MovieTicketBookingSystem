1286ec985f306428e237fb9c184aba818e6121cc
Kerem Can <keremcanozcan35@gmail.com>
2018-04-01 21:13:59 +0300

    MovieClass Updated

--- src/com/company/MovieClass.java
+++ src/com/company/MovieClass.java
@@ -1,5 +1,5 @@
 package com.company;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class MovieClass {
 
@@ -10,7 +10,7 @@ public class MovieClass {
    private int MovieIMDB;
    private String MovieDescription;
    private int MovieSeats;
-   ArrayList<String> MovieList;
+   //ArrayList<String> MovieList;
 
    public MovieClass(){
         MovieName=null;
@@ -82,7 +82,15 @@ public class MovieClass {
         System.out.println("Movie saloon has: " +getMovieSeats());
     }
 
-    public void currentMovieList(){
+    String[] MovieNames = new String[]{"Ready Player One","The Stolen Princess","Kaybedenler Kulubu","On Ikı Savascı", "Tomb Raider"};
+    String[] MovieTypes = new String[]{"Action", "Animation", "Drama", "War", "Action"};
+    String[] MovieDurations = new String[]{"120", "110","130", "100", "120"};
+    String[] MovieDirectors = new String[]{"Steven Spielberg", "Oleh Malamuzh", "Mehmet Ada Öztekin", "Nicolai Fuglsig", "Roar Uthaug"};
+    String[] MovieIMDBs = new String[]{"6.0","5.4","3.3","4.6","5.0"};
+    String[] MovieDescriptions = new String[]{"Description1", "Description2", "Description3", "Description4", "Description5"};
+    String[] MovieSeats = new String[]{"50", "50", "60", "60", "50"};
+
+    /*public void currentMovieList(){
         ArrayList<String> MovieList=new ArrayList<>(); //Creating Our Movie Array List
         System.out.println("Movie List is created.");
         MovieList.add("Movie1"); //Adding New Movies Into My Array List
@@ -90,7 +98,7 @@ public class MovieClass {
         MovieList.add("Movie3");
         MovieList.add("Movie4");
         MovieList.add("Movie5");
-    }
+    }*/
 
 
 
