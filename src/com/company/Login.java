c2ba38b353a682f09475f56c29848f9680b3d8a4
Kerem Can <keremcanozcan35@gmail.com>
2018-04-22 17:36:56 +0300

    Project Version2

--- src/com/company/Login.java
+++ src/com/company/Login.java
@@ -1,7 +1,20 @@
 package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
-public class Login {
+public class Login {  //This is the Login class; that the main screen of our program.
+
+    //  JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://80.211.16.77/movie";
+
+    //  Database credentials
+    static final String USER = "movieuser";
+    static final String PASS = "!!se!!proje";
 
     MovieClass movie = new MovieClass();
 
@@ -20,7 +33,7 @@ public class Login {
     private String moviedecision;
     private String seatdecision;
 
-    private void Menu(){
+    private void Menu() {  //This is the Menu class; that you can find selections of main menu.
         scan = new Scanner(System.in);
         System.out.println("Please select the sub-menu number: ");
         System.out.println("1- Add a new movie");
@@ -29,61 +42,135 @@ public class Login {
         decision = scan.next();
     }
 
-    private void CreateCustomer(){
-        scan = new Scanner(System.in);
-        System.out.println("Write your username: ");
-        newusername = scan.next();
-        System.out.println("Write your password: ");
-        newpassword = scan.next();
-        System.out.println("Write your mail adress: ");
-        newaddress = scan.next();
-    }
+    private void CreateCustomer() throws SQLException, ClassNotFoundException {  //This is the CreateCustomer class; that you can take customer's informations.
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        Scanner scanner = null;
+        //Class.forName("com.mysql.jdbc.Connection");
+        try {
+            Class.forName("com.mysql.jdbc.Connection");
+        } catch (Exception ex) {
 
-    private void LoginPrompt(){
-        scan = new Scanner(System.in);
-
-        System.out.println("Login");
-        System.out.println("username: ");
-        username = scan.next();
-
-        System.out.println("password: ");
-        password = scan.next();
-
-        if (username.equals("admin") && password.equals("adminpw")) {
-            System.out.println("Welcome, Admin!");
-            System.out.println("You are logged in");
-            System.out.println("Welcome the system");
-            Menu();
         }
 
-        if (username.equals("customer") && password.equals("customerpw")){
-            System.out.println("Welcome, Customer!");
-            System.out.println("You are logged in");
-            System.out.println("Welcome the system");
-            System.out.println("1 to buy New Tickets,\n2 to Show Current Tickets");
-            customerdecision = scan.next();
-            if (customerdecision.equals("1"))
-                movie.MovieDisplay();
-            System.out.println("Write your film name:");
-            // TODO: Check film name from the database.
-            moviedecision = scan.next();
-            System.out.println("Write your seat number:");
-            // TODO: Check the available seats.
-            System.out.println("_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _");
-            seatdecision = scan.next();
-            System.out.println("You are redirecting to the payment page...");
+        try {
+            scan = new Scanner(System.in);
+            System.out.println("Write your username: ");
+            newusername = scan.next();
+            System.out.println("Write your password: ");
+            newpassword = scan.next();
+            System.out.println("Write your mail adress: ");
+            newaddress = scan.next();
 
-            if (customerdecision.equals("2")) {
-                // TODO: Connect to DB and show the tickets.
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            String query = "insert into Register(user, pass, email)" + "values(?, ?, ?)";
+            stmt = conn.prepareStatement(query);
+
+            stmt.setString(1, newusername);
+            stmt.setString(2, newpassword);
+            stmt.setString(3, newaddress);
+            stmt.executeUpdate();
+
+            System.out.println("Insert complete.");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (scanner != null) {
+                scanner.close();
             }
         }
-        else{
-            System.out.println("Wrong Login! Please check your login informations!");
-            LoginPrompt();
+
+    }
+
+    private void LoginPrompt() throws SQLException, ClassNotFoundException {  //This is the LoginPrompt class; that you can check the both customer and admin names and passwords. And you can see the database connections.
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        Scanner scanner = null;
+        //Class.forName("com.mysql.jdbc.Connection");
+        try {
+            Class.forName("com.mysql.jdbc.Connection");
+        } catch (Exception ex) {
+
+        }
+        try {
+            scan = new Scanner(System.in);
+
+            System.out.println("Login");
+            System.out.println("username: ");
+            username = scan.next();
+
+            System.out.println("password: ");
+            password = scan.next();
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            String query = "SELECT * FROM Register WHERE user = ? AND pass = ?";
+            stmt = conn.prepareStatement(query);
+
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.executeQuery();
+
+            if (stmt.executeQuery().next()) {
+                if (username.equals("admin") && password.equals("adminpw")) {  //It checkes the admin's username and password.
+                    System.out.println("Welcome, Admin!");
+                    System.out.println("You are logged in");
+                    System.out.println("Welcome the system");
+                    Menu();
+                } else {      //It checks the customer's username and password.
+                    System.out.println("Welcome, " + username);
+                    System.out.println("You are logged in");
+                    System.out.println("Welcome the system");
+                    System.out.println("1 to buy New Tickets,\n2 to Show Current Tickets");
+                    customerdecision = scan.next();
+                    if (customerdecision.equals("1"))
+                        movie.MovieDisplay();
+                    System.out.println("Write your film name:");
+                    // TODO: Check film name from the database.
+                    moviedecision = scan.next();
+                    System.out.println("Write your seat number:");
+                    // TODO: Check the available seats.
+                    System.out.println("_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _\n_ _ _ _ _");
+                    seatdecision = scan.next();
+                    System.out.println("You are redirecting to the payment page...");
+
+                    if (customerdecision.equals("2")) {
+                        // TODO: Connect to DB and show the tickets.
+                    }
+                }
+            } else {
+                System.out.println("Wrong Login! Please check your login informations!");
+                LoginPrompt();
+            }
+
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (scanner != null) {
+                scanner.close();
+            }
         }
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {  //This is the our Main class.
+
 
         Login login = new Login();
 
--- src/com/company/MovieClass.java
+++ src/com/company/MovieClass.java
@@ -1,112 +1,105 @@
 package com.company;
-//import java.util.ArrayList;
 
-public class MovieClass {
 
-   private String MovieName;
-   private String MovieType;
-   private int MovieDuration;
-   private String MovieDirector;
-   private int MovieIMDB;
-   private String MovieDescription;
-   private int MovieSeats;
-   //ArrayList<String> MovieList;
+public class MovieClass {  //This is the MovieClass; that you can find the
+    //attributes of MovieClass' constructor, set and get methods. And you can find the movie information.
+    private String MovieName;
+    private String MovieType;
+    private int MovieDuration;
+    private String MovieDirector;
+    private int MovieIMDB;
+    private String MovieDescription;
+    private int MovieSeats;
+    //ArrayList<String> MovieList;
 
-   public MovieClass(){
-        MovieName=null;
-        MovieType=null;
-        MovieDuration=0;
-        MovieDirector=null;
-        MovieIMDB=0;
-        MovieDescription=null;
-        MovieSeats=0;
-    }
-
-   public String getMovieName(){
-       return MovieName;
-   }
-   public void setMovieName(String movieName){
-        this.MovieName=movieName;
-   }
-
-   public String getMovieType(){
-       return MovieType;
-   }
-   public void setMovieType(String movieType){
-       this.MovieType=movieType;
-   }
-
-   public int getMovieDuration(){
-       return MovieDuration;
-   }
-   public void setMovieDuration(int movieDuration){
-       this.MovieDuration=movieDuration;
-   }
-
-   public String getMovieDirector(){
-       return MovieDirector;
-   }
-   public void setMovieDirector(String movieDirector){
-       this.MovieDirector=movieDirector;
-   }
-
-   public int getMovieIMDB(){
-       return MovieIMDB;
-   }
-   public void setMovieIMDB(int movieIMDB){
-       this.MovieIMDB=movieIMDB;
-   }
-
-    public String getMovieDescription(){
-       return MovieDescription;
-    }
-    public void setMovieDescription(String movieDescription){
-       this.MovieDescription=movieDescription;
-    }
-
-    public int getMovieSeats(){
-       return MovieSeats;
-    }
-    public void setMovieSeats(int movieSeats){
-       this.MovieSeats=movieSeats;
+    public MovieClass() {  // This is the default constructor.
+        MovieName = null;
+        MovieType = null;
+        MovieDuration = 0;
+        MovieDirector = null;
+        MovieIMDB = 0;
+        MovieDescription = null;
+        MovieSeats = 0;
     }
 
 
-    public void MovieDisplay(){
-       for (int i=0;i<5;i++){
-           System.out.println("Movie name is: " +MovieNames[i]);
-           System.out.println("Movie type is: " +MovieTypes[i]);
-           System.out.println("Movie duration is: " +MovieDurations[i]);
-           System.out.println("Movie director is: " +MovieDirectors[i]);
-           System.out.println("Movie IMDB is: " +MovieIMDBs[i]);
-           System.out.println("Movie description is: " +MovieDescriptions[i]);
-           System.out.println("Movie saloon has: " +MovieSeat[i]);
-           System.out.println("---------------------");
-       }
-        /*System.out.println("Movie name is: " +getMovieName());
-        System.out.println("Movie type is: " +getMovieType());
-        System.out.println("Movie duration is: " +getMovieDuration());
-        System.out.println("Movie director is: " +getMovieDirector());
-        System.out.println("Movie IMDB is: " +getMovieIMDB());
-        System.out.println("Movie description is: " +getMovieDescription());
-        System.out.println("Movie saloon has: " +getMovieSeats());*/
+    public String getMovieName() {
+        return MovieName;
     }
 
-    String[] MovieNames = new String[]{"Ready Player One","The Stolen Princess","Kaybedenler Kulubu","On Ikı Savascı", "Tomb Raider"};
+    public void setMovieName(String movieName) {
+        this.MovieName = movieName;
+    }
+
+    public String getMovieType() {
+        return MovieType;
+    }
+
+    public void setMovieType(String movieType) {
+        this.MovieType = movieType;
+    }
+
+    public int getMovieDuration() {
+        return MovieDuration;
+    }
+
+    public void setMovieDuration(int movieDuration) {
+        this.MovieDuration = movieDuration;
+    }
+
+    public String getMovieDirector() {
+        return MovieDirector;
+    }
+
+    public void setMovieDirector(String movieDirector) {
+        this.MovieDirector = movieDirector;
+    }
+
+    public int getMovieIMDB() {
+        return MovieIMDB;
+    }
+
+    public void setMovieIMDB(int movieIMDB) {
+        this.MovieIMDB = movieIMDB;
+    }
+
+    public String getMovieDescription() {
+        return MovieDescription;
+    }
+
+    public void setMovieDescription(String movieDescription) {
+        this.MovieDescription = movieDescription;
+    }
+
+    public int getMovieSeats() {
+        return MovieSeats;
+    }
+
+    public void setMovieSeats(int movieSeats) {
+        this.MovieSeats = movieSeats;
+    }
+
+
+    public void MovieDisplay() {  //This is the function that we are showing the movies information.
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Movie name is: " + MovieNames[i]);
+            System.out.println("Movie type is: " + MovieTypes[i]);
+            System.out.println("Movie duration is: " + MovieDurations[i]);
+            System.out.println("Movie director is: " + MovieDirectors[i]);
+            System.out.println("Movie IMDB is: " + MovieIMDBs[i]);
+            System.out.println("Movie description is: " + MovieDescriptions[i]);
+            System.out.println("Movie saloon has: " + MovieSeat[i]);
+            System.out.println("---------------------");
+        }
+
+    }
+
+    String[] MovieNames = new String[]{"Ready Player One", "The Stolen Princess", "Kaybedenler Kulubu", "On Ikı Savascı", "Tomb Raider"};
     String[] MovieTypes = new String[]{"Action", "Animation", "Drama", "War", "Action"};
-    String[] MovieDurations = new String[]{"120", "110","130", "100", "120"};
+    String[] MovieDurations = new String[]{"120", "110", "130", "100", "120"};
     String[] MovieDirectors = new String[]{"Steven Spielberg", "Oleh Malamuzh", "Mehmet Ada Öztekin", "Nicolai Fuglsig", "Roar Uthaug"};
-    String[] MovieIMDBs = new String[]{"6.0","5.4","3.3","4.6","5.0"};
+    String[] MovieIMDBs = new String[]{"6.0", "5.4", "3.3", "4.6", "5.0"};
     String[] MovieDescriptions = new String[]{"Description1", "Description2", "Description3", "Description4", "Description5"};
     String[] MovieSeat = new String[]{"50", "50", "60", "60", "50"};
 
-    /*public void currentMovieList(){
-        ArrayList<String> MovieList=new ArrayList<>(); //Creating Our Movie Array List
-        System.out.println("Movie List is created.");
-        MovieList.add("Movie1"); //Adding New Movies Into My Array List
-        MovieList.add("Movie2");
-        MovieList.add("Movie3");
-        MovieList.add("Movie4");
-        MovieList.add("Movie5");
-    }*/
 }
